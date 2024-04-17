2e6a580ca71b106021ca23a5ad2b647094546e59
Nikki <nikecoes@gmail.com>
2024-04-17 15:33:23 +0200

    Added bongbongCommand (basically necogif from Necobot)

---
+++ src/commands/bongbongCommand.ts
@@ -0,0 +1,23 @@
+import {SlashCommand} from "../interfaces/slashCommand";
+import {CommandInteraction, EmbedBuilder, SlashCommandBuilder} from "discord.js";
+import config from "../config.json"
+
+
+export const command: SlashCommand = {
+    data: new SlashCommandBuilder()
+        .setName('bongbong')
+        .setDescription('Random bongbong gif!'),
+
+    async run(interaction: CommandInteraction) {
+
+        const color = "#003bb9";
+        const embed = new EmbedBuilder()
+            .setTitle('Bongbong!')
+            .setDescription('Thats me...!!!')
+            .setImage(config.bongGifs[Math.floor(Math.random() * config.bongGifs.length)])
+            .setColor(color)
+
+        interaction.reply({embeds: [embed]});
+    }
+
+}

7719ef50070d24e649f0c752d5c5a15fd7276ff9
Nikki <nikecoes@gmail.com>
2024-04-17 12:33:34 +0200

    Removed unused imports, removed unused event collection

--- src/classes/botClient.ts
+++ src/classes/botClient.ts
@@ -1,8 +1,7 @@
-import {Client, Collection, Events, GatewayIntentBits} from "discord.js";
+import {Client, Collection, GatewayIntentBits} from "discord.js";
 import config from "../config.json";
 import {SlashCommand} from "../interfaces/slashCommand";
 import {getSlashCommands} from "../handlers/commandHandler";
-import {DiscordEvent} from "../interfaces/discordEvent";
 import {registerEvents} from "../handlers/eventHandler";
 
 export default class BotClient {
@@ -16,7 +15,6 @@ export default class BotClient {
     });
 
     slashCommands: Collection<string, SlashCommand> = new Collection<string, SlashCommand>();
-    events: Collection<string, DiscordEvent> = new Collection<string, DiscordEvent>()
 
     async start() {
 

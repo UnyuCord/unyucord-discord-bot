cd93f58cde09a1f0fa77a78e4a93faab234ab73c
Nikki <nikecoes@gmail.com>
2024-04-18 15:37:07 +0200

    Tenor link instead of discord link for bongbong explosion gif (thx missa and des)

--- src/handlers/errorHandler.ts
+++ src/handlers/errorHandler.ts
@@ -3,11 +3,10 @@ import {CommandInteraction, EmbedBuilder} from "discord.js";
 
 export function sendErrorEmbed(interaction: CommandInteraction, errorDescription: string) {
 
-    // TODO: This url will expire some day, maybe store gif locally?
     const errorEmbed = new EmbedBuilder()
         .setColor('Red')
         .setTitle(':boom: BONGBONG HAS JUST EXPLODED!!! :boom:')
-        .setImage("https://cdn.discordapp.com/attachments/1083826396314480742/1230149314836434945/3dgifmaker75955.gif?ex=663244d3&is=661fcfd3&hm=b49d349d923266fba726163f593751779f289ee1d30217776f03337b60fe12ac&")
+        .setImage("https://media.tenor.com/--tErbWBQ3AAAAAi/bongbong-explosion.gif")
         .setDescription(errorDescription)
         .setFooter({text: 'Blame Nikki for this...'})
 

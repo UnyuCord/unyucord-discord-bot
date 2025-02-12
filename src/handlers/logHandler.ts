1c8a4db323fb0c8eb2c9487328728b6306bf9833
Nikki <nikecoes@gmail.com>
2025-02-12 03:26:33 +0100

    Why the hell did I use UTC

--- src/handlers/logHandler.ts
+++ src/handlers/logHandler.ts
@@ -2,21 +2,21 @@
 import {AnsiEscapeColors} from "../resources/ansiEscapeColors";
 
 export function logInfo(content: string) {
-    const timestamp = new Date().toLocaleString('en-GB', {timeZone: 'UTC'});
+    const timestamp = new Date().toLocaleString('en-GB', {timeZone: 'CET'});
     console.info(`[${timestamp}] ` + content);
 }
 
 export function logWarning(content: string) {
-    const timestamp = new Date().toLocaleString('en-GB', {timeZone: 'UTC'});
+    const timestamp = new Date().toLocaleString('en-GB', {timeZone: 'CET'});
     console.warn(`[${timestamp}] ` + AnsiEscapeColors.Yellow + content + AnsiEscapeColors.Reset);
 }
 
 export function logError(content: string) {
-    const timestamp = new Date().toLocaleString('en-GB', {timeZone: 'UTC'});
+    const timestamp = new Date().toLocaleString('en-GB', {timeZone: 'CET'});
     console.error(`[${timestamp}] ` + AnsiEscapeColors.Red + content + AnsiEscapeColors.Reset);
 }
 
 export function logSuccess(content: string) {
-    const timestamp = new Date().toLocaleString('en-GB', {timeZone: 'UTC'});
+    const timestamp = new Date().toLocaleString('en-GB', {timeZone: 'CET'});
     console.info(`[${timestamp}] ` + AnsiEscapeColors.Green + content + AnsiEscapeColors.Reset);
 }
